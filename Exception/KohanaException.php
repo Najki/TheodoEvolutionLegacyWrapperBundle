d630c28e174f52fb4276bc108ae3acfefa8ba9e6
Nikodem <najki@MacBook-Pro-Nikodem.local>
2016-11-03 23:41:46 +0100

    Przygotowanie do Kohana kernel

---
+++ Exception/KohanaException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Theodo\Evolution\Bundle\LegacyWrapperBundle\Exception;
+
+/**
+ * CodeIgniterException
+ *
+ * @author Benjamin Grandfond <benjaming@theodo.fr>
+ */
+class KohanaException extends \RuntimeException
+{
+
+}
